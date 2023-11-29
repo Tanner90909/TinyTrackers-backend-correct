cfdcca3ae52c4cd0da054d5334718d9093e4519d
Tanner <tmmcdaniel909@gmail.com>
2023-11-29 20:22:31 +0000

    schemas

--- app/schemas/child.py
+++ app/schemas/child.py
@@ -1,6 +1,12 @@
 from typing import Optional
+from sqlalchemy import Date
 
 from pydantic import BaseModel, EmailStr
 
 class ChildBase(BaseModel):
-    
+    first_name: str
+    last_name: str
+    dob: Date
+    allergies: str
+    pediatrician_name: str
+    pediatrician_number: str
